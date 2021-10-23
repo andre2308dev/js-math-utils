9e346ba97960671983ccb21094e5153059e41e5b
andres_arenas <osorioandre204@gmail.com>
2021-10-23 02:09:48 -0500

    Hice función Multiplicar

--- Ejercicio-01/main.js
+++ Ejercicio-01/main.js
@@ -1,11 +1,15 @@
 const suma = (a,b) => {
-    console.log(a+b);
+    console.log(a + b);
 };
 
 suma(10,5);
 
 const resta = (a,b) => {
-    console.log(a - b );
+    console.log(a - b);
 };
 
 resta(100,50);
+
+(function (a,b) {
+    console.log(a * b);
+})(6,5)
