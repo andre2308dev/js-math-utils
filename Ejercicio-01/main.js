a8adc38b7a8a58654dc39d077ce46076e0ef95fb
andres_arenas <osorioandre204@gmail.com>
2021-10-25 00:24:32 -0500

    Agregamos división

--- Ejercicio-01/main.js
+++ Ejercicio-01/main.js
@@ -13,3 +13,13 @@ resta(100,50);
 (function (a,b) {
     console.log(a * b);
 })(6,5)
+
+const division = (a,b) => {
+    try {return console.log(a/b)}
+    catch(err) {
+        console.log(err)
+    }
+    finally {return 0}
+}
+
+division(200,2);
